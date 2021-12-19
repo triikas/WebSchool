e6db5f825291d470d95f11b80d746be4b5a15270
Tri <triikas@triikas.ru>
2021-12-19 23:52:54 +0300

    mod settings

--- WebSchool/settings.py
+++ WebSchool/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
